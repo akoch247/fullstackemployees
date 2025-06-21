25f1a4d8d8afb86799661c41a3ab8af5d85cfa04
Adriana Koch <akoch@oberlin.edu>
2025-06-21 11:44:19 -0400

    finish step 1-4

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,9 @@
 DROP TABLE IF EXISTS employees;
 
+CREATE TABLE employees (
+    id serial PRIMARY KEY,
+    name text,
+    birthday date,
+    salary integer );
+
 
--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,5 @@
 import db from "#db/client";
+import { createEmployee } from "./queries/employees";
 
 await db.connect();
 await seedEmployees();
@@ -6,5 +7,63 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
-  // TODO
+  await createEmployee({
+    name: "Steve",
+    birthday: "1981-01-01",
+    salary: 75000,
+  });
+
+  await createEmployee({
+    name: "Steven",
+    birthday: "1961-01-01",
+    salary: 75500,
+  });
+
+  await createEmployee({
+    name: "Stevie",
+    birthday: "1989-01-01",
+    salary: 75550,
+  });
+
+  await createEmployee({
+    name: "Stev",
+    birthday: "1987-01-01",
+    salary: 75001,
+  });
+
+  await createEmployee({
+    name: "Stephen",
+    birthday: "1951-01-01",
+    salary: 75600,
+  });
+
+  await createEmployee({
+    name: "Stephan",
+    birthday: "1971-01-01",
+    salary: 75030,
+  });
+
+  await createEmployee({
+    name: "Steph",
+    birthday: "1941-01-01",
+    salary: 70000,
+  });
+
+  await createEmployee({
+    name: "Stephanie",
+    birthday: "1981-02-01",
+    salary: 71000,
+  });
+
+  await createEmployee({
+    name: "Stefan",
+    birthday: "1921-01-01",
+    salary: 66000,
+  });
+
+  await createEmployee({
+    name: "Stefano",
+    birthday: "1991-01-01",
+    salary: 95000,
+  });
 }
